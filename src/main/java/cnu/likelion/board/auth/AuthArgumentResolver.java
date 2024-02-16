4e6411d8752e7d891696d028629d376fba8215ee
shin-mallang <huipulci1@naver.com>
2024-02-17 01:01:10 +0900

    step 3 complete

---
+++ src/main/java/cnu/likelion/board/auth/AuthArgumentResolver.java
@@ -0,0 +1,48 @@
+package cnu.likelion.board.auth;
+
+import static java.util.Objects.requireNonNull;
+
+import cnu.likelion.board.auth.jwt.JwtService;
+import cnu.likelion.board.common.exception.UnAuthorizedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtService jwtService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(Auth.class)
+               && parameter.getParameterType().equals(Long.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory
+    ) {
+        Auth authAt = parameter.getParameterAnnotation(Auth.class);
+        requireNonNull(authAt);
+        String accessToken = extractAccessToken(webRequest);
+        return jwtService.extractMemberId(accessToken);
+    }
+
+    private static String extractAccessToken(NativeWebRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        throw new UnAuthorizedException("로그인 후 접근할 수 있습니다.");
+    }
+}
