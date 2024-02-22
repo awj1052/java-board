a8a8bb83720d6028b0411304a807a0cf38f0f1e7
awj1052 <awj1052@naver.com>
2024-02-23 05:19:27 +0900

    step2 complete

--- src/main/java/cnu/likelion/board/member/presentation/MemberController.java
+++ src/main/java/cnu/likelion/board/member/presentation/MemberController.java
@@ -1,5 +1,6 @@
 package cnu.likelion.board.member.presentation;
 
+import cnu.likelion.board.auth.jwt.JwtService;
 import cnu.likelion.board.member.application.MemberService;
 import cnu.likelion.board.member.presentation.request.LoginRequest;
 import cnu.likelion.board.member.presentation.request.MemberSignupRequest;
@@ -19,8 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
-
-    // TODO [2단계] JwtService 를 의존성 주입 받으세요.
+    private final JwtService jwtService;
 
     @PostMapping
     public ResponseEntity<Void> signup(
@@ -30,14 +30,12 @@ public class MemberController {
         return ResponseEntity.created(URI.create("/members/" + memberId)).build();
     }
 
-    // TODO [2단계] [ POST , /members/login ] 로 들어오는 요청에 대해 동작해야 합니다.
+    @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(
-            // TODO [2단계] Json 타입으로 들어오는 request Body를 매핑합니다.
-            LoginRequest request
+            @Valid @RequestBody LoginRequest request
     ) {
-        // TODO [2단계] 회원 서비스를 사용하여 로그인을 진행합니다.
-        // TODO [2단계] 로그인 결과 반환된 Id를 가지고 Jwt(AccessToken)를 생성합니다.
-        // TODO [2단계] 생성한 accessToken을 LoginResponse로 감싸 반환합니다.
-        return null;
+        Long memberId = memberService.login(request.username(), request.password());
+        String accessToken = jwtService.createToken(memberId);
+        return ResponseEntity.ok(new LoginResponse(accessToken));
     }
 }
