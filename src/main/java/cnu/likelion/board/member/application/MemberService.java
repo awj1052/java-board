a2b2f8585bd72ebdbed1c6eab15dce5d7481a104
awj1052 <awj1052@naver.com>
2024-02-23 03:43:27 +0900

    feat: MemberService

--- src/main/java/cnu/likelion/board/member/application/MemberService.java
+++ src/main/java/cnu/likelion/board/member/application/MemberService.java
@@ -1,5 +1,6 @@
 package cnu.likelion.board.member.application;
 
+import cnu.likelion.board.common.exception.UnAuthorizedException;
 import cnu.likelion.board.member.domain.Member;
 import cnu.likelion.board.member.domain.MemberRepository;
 import cnu.likelion.board.member.domain.MemberValidator;
@@ -27,10 +28,9 @@ public class MemberService {
             String username,
             String password
     ) {
-        // TODO [2단계] 저장소에서 주어진 username이라는 아이디를 갖는 회원을 찾습니다.
-        // TODO [2단계] 해당 회원이 없다면 테스트를 참고하여 예외를 발생시킵니다.
-        // TODO [2단계] 회원을 찾아왔다면 로그인을 진행합니다. 이때 비밀번호 비교 로직은 이곳에서 진행하지 말고 member 객체에게 시킵니다.
-        // TODO [2단계] 로그인에 성공하면 회원 id를 반환합니다.
-        return null;
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UnAuthorizedException("존재하지 않는 아이디입니다."));
+        member.login(password);
+        return member.getId();
     }
 }
