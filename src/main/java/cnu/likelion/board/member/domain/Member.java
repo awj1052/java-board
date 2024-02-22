cb85a4b41ccd1e40a60c02a259047338eb54c321
awj1052 <awj1052@naver.com>
2024-02-23 02:55:48 +0900

    Feat: Member, MemberValidator

--- src/main/java/cnu/likelion/board/member/domain/Member.java
+++ src/main/java/cnu/likelion/board/member/domain/Member.java
@@ -21,6 +21,6 @@ public class Member {
     }
 
     public void signup(MemberValidator validator) {
-        // TODO [1단계] validator를 통해 닉네임 중복 검증을 진행하세요
+        validator.validateDuplicatedUsername(this.username);
     }
 }
--- src/main/java/cnu/likelion/board/member/domain/MemberValidator.java
+++ src/main/java/cnu/likelion/board/member/domain/MemberValidator.java
@@ -6,14 +6,13 @@ import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-// TODO [1단계] Component 빈으로 등록하세요
 public class MemberValidator {
 
     private final MemberRepository memberRepository;
 
     public void validateDuplicatedUsername(String username) {
-        // TODO [1단계] MemberRepository 의 특정 메서드를 사용하여,
-        // TODO [1단계] 아이디의 중복을 검사하세요.
-        // TODO [1단계] 중복된다면 테스트를 참고하여 적절한 예외와 메세지를 반환하세요.
+        if(memberRepository.findByUsername(username).isPresent()) {
+            throw new ConflictException("이미 사용중인 아이디입니다.");
+        }
     }
 }
