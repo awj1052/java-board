b56df810d258a707e5a3cb05ae650598086e221d
awj1052 <awj1052@naver.com>
2024-02-23 02:52:46 +0900

    Feat: MemberRepository

--- src/main/java/cnu/likelion/board/member/domain/MemberRepository.java
+++ src/main/java/cnu/likelion/board/member/domain/MemberRepository.java
@@ -7,7 +7,6 @@ import java.util.Map.Entry;
 import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
-// TODO [1단계] Repository 빈으로 등록하세요.
 @Repository
 public class MemberRepository {
 
@@ -15,21 +14,20 @@ public class MemberRepository {
     private Long sequence = 1L;
 
     public Member save(Member member) {
-        // TODO [1단계] member에 Id가 중복되지 않도록 세팅하고,
-        // TODO [1단계] members 에 id를 Key로, member를 value로 저장하세요.
-        return null;
+        member.setId(sequence++);
+        members.put(member.getId(), member);
+        return member;
     }
 
     public Optional<Member> findById(Long id) {
-        // TODO [1단계] 저장된 회원 중 id가 일치하는 회원을 찾아 Optional 로 감싸서 반환하세요.
-        // TODO [1단계] 만약 id가 일치하는 회원이 없는 경우, 빈 Optional 객체를 반환하세요.
-        return null;
+        return Optional.ofNullable(members.get(id));
     }
 
     public Optional<Member> findByUsername(String username) {
-        // TODO [1단계] 저장된 회원 중 username이 일치하는 회원을 찾아 Optional 로 감싸서 반환하세요.
-        // TODO [1단계] 만약 username이 일치하는 회원이 없는 경우, 빈 Optional 객체를 반환하세요.
-        return null;
+        return members.values()
+                .stream()
+                .filter(value -> value.getUsername().equals(username))
+                .findAny();
     }
 
     // for test
