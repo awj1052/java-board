035e0ab4276bd5c48119e6b196985374f2afc8a7
awj1052 <awj1052@naver.com>
2024-02-25 13:09:43 +0900

    step2 complete

--- src/main/java/cnu/likelion/board/auth/jwt/JwtService.java
+++ src/main/java/cnu/likelion/board/auth/jwt/JwtService.java
@@ -22,21 +22,16 @@ public class JwtService {
         this.objectMapper = new ObjectMapper();
         this.accessTokenExpirationDayToMills =  // 요일을 밀리초로 바꾸는 방법입니다. 참고만 합니다.
                 MILLISECONDS.convert(jwtProperty.accessTokenExpirationDay(), DAYS);
-
-        // TODO [2단계] jwtProperty의 secretKey 를 HMAC512 로 암호화합니다.
-        // TODO [2단계] 이를 위해 com.auth0.jwt.algorithms.Algorithm 의 적절한 메서드를 사용하세요.
-        this.algorithm = null;
+        this.algorithm = Algorithm.HMAC256(jwtProperty.secretKey());
     }
 
     public String createToken(Long memberId) {
         return JWT.create()
                 .withExpiresAt(new Date(
-                        // TODO [2단계] 토큰의 만료일을 설정합니다. 만료일은 설정한 액세스 토큰 만료 기간(밀리초) + 현재시간(밀리초)
+                        accessTokenExpirationDayToMills + System.currentTimeMillis()
                 ))
                 .withIssuedAt(new Date())  // 현재 시간을 발급일로 설정합니다.
-
-                // TODO [2단계] memberId 클레임을 설정합니다. 값은 인자로 주어진 회원 Id로 설정합니다.
-                .withClaim("TODO", "TODO")
+                .withClaim("memberId", memberId)
                 .sign(algorithm); // 알고리즘으로 서명합니다.
     }
 
@@ -45,7 +40,7 @@ public class JwtService {
             return JWT.require(algorithm) // 주어진 토큰이 우리가 설정한 secretkey로 서명되었는지 확인을 위해 필요합니다.
                     .build()
                     .verify(token) // 토큰이 유효한지 검사합니다.
-                    .getClaim("TODO") // TODO [2단계] 토큰에서 회원 Id 정보를 담은 클래임을 가져옵니다.
+                    .getClaim("memberId")
                     .asLong();
         } catch (JWTVerificationException e) {
             throw new UnAuthorizedException("유효하지 않은 토큰입니다.");
