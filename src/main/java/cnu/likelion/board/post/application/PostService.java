998c9ac621d74c4ecb0b514154ecc16225237c80
awj1052 <awj1052@naver.com>
2024-02-25 21:23:58 +0900

    feat: Post

--- src/main/java/cnu/likelion/board/post/application/PostService.java
+++ src/main/java/cnu/likelion/board/post/application/PostService.java
@@ -1,5 +1,10 @@
 package cnu.likelion.board.post.application;
 
+import cnu.likelion.board.common.exception.UnAuthorizedException;
+import cnu.likelion.board.member.domain.Member;
+import cnu.likelion.board.member.domain.MemberRepository;
+import cnu.likelion.board.post.domain.Post;
+import cnu.likelion.board.post.domain.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,13 +12,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class PostService {
 
+    private final PostRepository postRepository;
+    private final MemberRepository memberRepository;
+
     public Long write(
             Long memberId,
             String title,
             String content
     ) {
-        // TODO [4단계] 포스트를 작성하는 로직을 작성하시오. 생성된 포스트 Id를 반환합니다.
-        return null;
+        Member writer = memberRepository.findById(memberId)
+                .orElseThrow(() -> new UnAuthorizedException("회원 정보가 존재하지 않습니다"));
+        Post post = new Post(title, content, writer);
+        return postRepository.save(post).getId();
     }
 
     public void update(
@@ -22,15 +32,23 @@ public class PostService {
             String title,
             String content
     ) {
-        // TODO [4단계] 포스트 수정 로직을 작성하시오.
-        // TODO [4단계] 수정 전 포스트에 대한 접근 권한을 확인해야 합니다.
+        Member writer = memberRepository.findById(memberId)
+                .orElseThrow(() -> new UnAuthorizedException("회원 정보가 존재하지 않습니다"));
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new UnAuthorizedException("포스트 정보가 존재하지 않습니다"));
+        post.validateAuthority(writer);
+        post.update(title, content);
     }
 
     public void delete(
             Long memberId,
             Long postId
     ) {
-        // TODO [4단계] 포스트 삭제 로직을 작성하시오.
-        // TODO [4단계] 삭제 전 포스트에 대한 접근 권한을 확인해야 합니다.
+        Member writer = memberRepository.findById(memberId)
+                .orElseThrow(() -> new UnAuthorizedException("회원 정보가 존재하지 않습니다"));
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new UnAuthorizedException("포스트 정보가 존재하지 않습니다"));
+        post.validateAuthority(writer);
+        postRepository.delete(post);
     }
 }
--- src/main/java/cnu/likelion/board/post/domain/PostRepository.java
+++ src/main/java/cnu/likelion/board/post/domain/PostRepository.java
@@ -1,6 +1,6 @@
 package cnu.likelion.board.post.domain;
 
-public interface PostRepository {
-    // TODO [5단계] 기존 코드를 모두 지우고, JpaRepository 를 사용해보자.
-    // TODO [5단계] 해당 코드의 변경 외에는 변경되는 코드 없이 모든 코드가 잘 동작해야 한다.
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
