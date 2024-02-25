a1993a73e8d319e9e6107e73e21b5fcacc5b8c8e
awj1052 <awj1052@naver.com>
2024-02-25 12:59:42 +0900

    step1 complete

--- src/main/java/cnu/likelion/board/member/application/MemberService.java
+++ src/main/java/cnu/likelion/board/member/application/MemberService.java
@@ -1,19 +1,26 @@
 package cnu.likelion.board.member.application;
 
-// TODO [1단계] Service 빈으로 등록하세요
+import cnu.likelion.board.member.domain.Member;
+import cnu.likelion.board.member.domain.MemberRepository;
+import cnu.likelion.board.member.domain.MemberValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class MemberService {
 
-    // TODO [1단계] MemberRepository와, MemberValidator를 의존성 주입 받습니다.
-
+    private final MemberRepository memberRepository;
+    private final MemberValidator memberValidator;
     public Long signup(
             String username,
             String password,
             String name
     ) {
-        // TODO [1단계] 회원 객체를 만들고, 회원가입을 진행하세요.
-        // TODO [1단계] 회원가입 시 아이디 중복 체크는 이곳에서 하지 말고, 회원의 메서드를 통해 진행되도록 합니다.
-        // TODO [1단계] 중복 체크 이후 회원 정보를 저장하고, 저장된 회원의 id를 반환합니다.
-        return null;
+        Member member = new Member(username, password, name);
+        member.signup(memberValidator);
+        Member saved = memberRepository.save(member);
+        return saved.getId();
     }
 
     public Long login(
