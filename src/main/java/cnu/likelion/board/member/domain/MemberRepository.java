56f72d36851d4af1ad4d7832f52947052d12be2f
awj1052 <awj1052@naver.com>
2024-02-25 20:59:29 +0900

    feat:  MemberRepository

--- src/main/java/cnu/likelion/board/member/domain/MemberRepository.java
+++ src/main/java/cnu/likelion/board/member/domain/MemberRepository.java
@@ -1,7 +1,9 @@
 package cnu.likelion.board.member.domain;
 
-public interface MemberRepository {
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-    // TODO [5단계] 기존 코드를 모두 지우고, JpaRepository 를 사용해보자.
-    // TODO [5단계] 해당 코드의 변경 외에는 변경되는 코드 없이 모든 코드가 잘 동작해야 한다.
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
 }
